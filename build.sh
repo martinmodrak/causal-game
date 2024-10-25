f04d14fc82e901835fdc46ef6b689fe0c2a0048c
Martin Modrák <modrak.mar@gmail.com>
2024-10-25 05:09:12 +0200

    Build

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-elm make src/Main.elm --output=docs/elm.js --optimize
+elm make src/Main.elm --output=docs/elm-big.js --optimize
+minify -o docs/elm.js docs/elm-big.js
+rm docs/elm-big.js
 cp index.html docs
 cp style.css docs
+
